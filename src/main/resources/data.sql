cca7f10a092cb683c9965980586b28caf866b9b3
moehaji <mo.ali.abdulla@gmail.com>
2022-10-12 12:32:10 -0400

    data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
-insert into product (productId, deptId,productName,deptName) values (1,2,'milk', 'dairy'),(2,3,'chocolate','candy');
+insert into product (productId, deptId,productName,deptName) values
+(1,1,'T-Shirt', 'Clothing'),
+(2,1,'Jeans','Clothing'),
+(3,2,'Camera','Electronics'),
+(4,2,'T.V.','Electronics'),
+(5,2,'Computer','Electronics'),
+(6,3,'Watch','Accessories'),
+(7,3,'Cap','Accessories'),
+(8,4,'Orange','Food'),
+(9,4,'Chocolate Bar','Food'),
+(10,4,'Chips','Food');
